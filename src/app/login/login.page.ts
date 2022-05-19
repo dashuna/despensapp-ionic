35e88d33bd0b33c10b53da2e536234b4f30959cd
Dasha <darya.lynnyk@juanxxiii.net>
2022-05-19 16:51:09 +0200

    cambios login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { SERVER_URL } from '../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,17 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 
+//const AUTH_URL = "/authenticate";
 export class LoginPage implements OnInit {
   email: string = "";
   password: string = "";
 
-  constructor() { }
+  constructor(public httpClient: HttpClient) { }
 
   ngOnInit() { }
 
   onSubmit() {
-    alert(
-     this.email + ', ' + this.password
-    )
+    let postData = {
+      "username": this.email,
+      "password": this.password
+    }
+
+    this.httpClient.post(SERVER_URL + "/authenticate", postData)
+      .subscribe(data => {
+        console.log(data);
+       }, error => {
+        console.log(error);
+      });
  }
 }
